ab32d586ebf050183e6c7341eeb086641606dd3e
Chris Curran <chriscurran8@yahoo.com>
2020-12-21 19:47:08 +0000

    add materialize navbar and side bar

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+  $(document).ready(function(){
+    $('.sidenav').sidenav({edge: "right"});
+  });
+
+//   $(body).css("background-color:", "brown");
+
+
+//   document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+//   });
